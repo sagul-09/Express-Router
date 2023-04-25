4689dcc4f3e576dcf972fb16b36790db36893dc7
Sagul-09 <nihal.evil.royal@gmail.com>
2023-04-25 20:17:05 +0530

    changes in services file

--- routes/services.js
+++ routes/services.js
@@ -2,7 +2,7 @@ import express from "express";
 const route = express.Router();
 
 route.get("/", (req, res) => {
-  res.send("FIND SERVICES BY ID");
+  res.send("FIND SERVICES BY ID FROM 1-4");
 });
 
 let listServices = [
@@ -26,7 +26,6 @@ let listServices = [
     services: "App development",
   },
 ];
-
 route.get("/:id", (req, res) => {
   const id = Number(req.params.id);
 
@@ -35,8 +34,25 @@ route.get("/:id", (req, res) => {
   if (output) {
     res.send("The Service required is from the " +"'"+output.services+"'"+ " Domain");
   } else {
-    res.send(500);
+    res.send("INVALID SERVICE");
   }
 });
 
+
+
 export default route;
+
+// route.get("/", (req, res) => {
+//   res.status(200).send(res.json({ services }));
+// });
+
+// route.get("/:id", (req, res) => {
+//   res.send("Get services with id: " + req.params.id);
+//   console.log(req.service.name);
+// });
+
+// route.param("id", (req, res, next, id) => {
+//   console.log(req.params.id);
+//   req.service = services[id];
+//   next();
+// });
